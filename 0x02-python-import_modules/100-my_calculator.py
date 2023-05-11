c0e27ea9983ad84ddd9913479739c6508b242ac6
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-12 00:33:58 +0300

    Task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-
     if (sys.argv[2] == '+'):
         print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
     elif (sys.argv[2] == '-'):
