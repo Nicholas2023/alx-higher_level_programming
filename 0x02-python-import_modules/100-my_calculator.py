2b0be229f6d60e1d7d49e5d76135570614c4e4ab
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-12 00:37:38 +0300

    Task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    if (sys.argv[2] == '+'):
+    if (sys.argv[2] == "+"):
         print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
-    elif (sys.argv[2] == '-'):
+    elif (sys.argv[2] == "-"):
         print("{} {} {} = {}".format(a, sys.argv[2], b, sub(a, b)))
-    elif (sys.argv[2] == '*'):
+    elif (sys.argv[2] == "*"):
         print("{} {} {} = {}".format(a, sys.argv[2], b, mul(a, b)))
-    elif (sys.argv[2] == '/'):
+    elif (sys.argv[2] == "/"):
         print("{} {} {} = {}".format(a, sys.argv[2], b, div(a, b)))
     else:
         print("Unknown operator. Available operator: +, -, * and /")
