f3a211afe3cf50fe8ec8b77728507022d09a19a1
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-12 00:22:02 +0300

    Task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,8 +3,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    args = sys.argv[1:]
-    num_args = len(args)
+    num_args = len(sys.argv) - 1
 
     if (num_args != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
