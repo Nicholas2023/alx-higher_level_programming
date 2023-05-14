edc03b5b85cf61db45d20fc0ac358057b6b7e846
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-14 14:46:11 +0300

    Task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    list_range = len(my_list) - 1
+    if ((idx < 0) or (idx > list_range)):
+        return (my_list)
+    else:
+        my_list[idx] = element
+        return (my_list)
