989a424cdcc50b295aee2a8102391532610c2620
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-14 14:51:32 +0300

    Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    '''If the index is negative or out of range'''
     list_range = len(my_list) - 1
     if ((idx < 0) or (idx > list_range)):
-        return (my_list)
+        return (my_list.copy())
     else:
-        my_list[idx] = element
-        return (my_list)
+        '''Create a copy of the original list'''
+        '''Replace the elements at a given inddex'''
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return (new_list)
