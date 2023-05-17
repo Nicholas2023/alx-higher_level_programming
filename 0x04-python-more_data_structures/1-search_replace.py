31fe344bba5e93feb89a7550a1da409544da17de
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-18 01:31:35 +0300

    p

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,9 @@
 #!/usr/bin/pyhton3
 def search_replace(my_list, search, replace):
     new_list = []
-    for i, val in enumerate(my_list):
-        if val != search:
-            new_list.append(val)
-        else: 
-            my_list[i] = replace
+    for i in my_list:
+        if i == search:
             new_list.append(replace)
-        
+        else:
+            new_list.append(i)
     return new_list
