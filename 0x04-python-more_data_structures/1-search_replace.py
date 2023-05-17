7823c9d1f440fab3e2f33cee53253eff3e37032b
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-18 01:34:41 +0300

    p

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
-    new_list = []
-    for i in my_list:
-        if i == search:
-            new_list.append(replace)
-        else:
-            new_list.append(i)
-    return new_list
+    """Replace all occurrences of an element by another in a new list."""
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
