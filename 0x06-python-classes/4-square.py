27bedbb9b4fdcbac31b8beb63046dec45e826447
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-31 00:59:51 +0300

    task4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Class Definition'''
+
+
+class Square:
+    '''Attributes'''
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size ** 2
