806ff31eb016e4a115633809d5b3c9df6997fef0
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-06-05 23:47:30 +0300

    task0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    This module creates an empty class called Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Represents a rectangle shape.
+
+    Attributes:
+        No attributes defined at the moment.
+    """
+
+    pass
