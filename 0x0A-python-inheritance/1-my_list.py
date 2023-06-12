74c8d71572e89b3c7bdd13ad4c79151293f88459
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-06-12 21:43:32 +0300

    task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    This module defines the MyList class, which is a subclass of list
+"""
+
+
+class MyList(list):
+    """
+        A subclass of list that provides additional functionality
+        for printing the list in sorted order
+    """
+
+    def print_sorted(self):
+        """
+            Prints the list in sorted order
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
