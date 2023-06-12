2a15d0225505cf4517bae2dd0e185a655a2f9491
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-06-13 01:15:32 +0300

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Defines the BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    def area(self):
+        """
+        Calculate the area of the geometry
+        Raises:
+            Exception: Indicates that the `area()` method
+                    is not implemented for the geometry
+        Returns:
+            None
+        """
+        raise Exception("Area () is not implemented")
