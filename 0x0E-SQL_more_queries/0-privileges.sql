3d96622b1a1a0a51ccb72fab933fd5a1ff0cf1b2
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-07-20 00:46:20 +0300

    privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- Lists all privileges of the users user_0d_1 and user_0d_2.
-
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SELECT User, Host, Grant_priv, Create_priv, Insert_priv, Update_priv, Delete_priv, Select_priv
+FROM mysql.user
+WHERE User IN ('user_0d_1', 'user_0d_2')
+    AND Host = 'localhost';
