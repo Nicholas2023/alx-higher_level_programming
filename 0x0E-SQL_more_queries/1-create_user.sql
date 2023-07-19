6f2ead15e47230d20a3a5222f29615cf1560c538
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-07-20 01:10:26 +0300

    user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Creates the MYSQL server user_0d_1
+
+-- user_0d_1 should have all privileges on mysql server
+-- user_0d_1 password should bet set to user_0d_1_pwd
+-- if user_0d_1 already exists, the script should not fail
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
