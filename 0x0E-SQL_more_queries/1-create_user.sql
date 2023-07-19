29808885c5d3f0bb6b8f5c7d97dd6a624c2c3072
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-07-20 01:17:29 +0300

    user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,5 +4,5 @@
 -- user_0d_1 password should bet set to user_0d_1_pwd
 -- if user_0d_1 already exists, the script should not fail
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
