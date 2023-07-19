6a46bed8f66f6946ebe47de3368366e0346aa100
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-07-20 01:12:13 +0300

    user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,4 +5,4 @@
 -- if user_0d_1 already exists, the script should not fail
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
