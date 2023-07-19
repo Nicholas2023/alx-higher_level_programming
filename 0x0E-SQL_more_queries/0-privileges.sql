94a8b947bdd85b247a86b941bbfd6ee85e27a578
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-07-20 00:36:10 +0300

    privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,27 @@
--- List privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Check privileges for user_0d_1
+SELECT 
+  USER, 
+  HOST, 
+  MAX_USER_CONNECTIONS,
+  MAX_QUERIES_PER_HOUR,
+  MAX_UPDATES_PER_HOUR,
+  MAX_CONNECTIONS_PER_HOUR
+FROM 
+  mysql.user
+WHERE 
+  USER = 'user_0d_1' AND
+  HOST = 'localhost';
 
--- List privileges for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Check privileges for user_0d_2
+SELECT 
+  USER, 
+  HOST, 
+  MAX_USER_CONNECTIONS,
+  MAX_QUERIES_PER_HOUR,
+  MAX_UPDATES_PER_HOUR,
+  MAX_CONNECTIONS_PER_HOUR
+FROM 
+  mysql.user
+WHERE 
+  USER = 'user_0d_2' AND
+  HOST = 'localhost';
