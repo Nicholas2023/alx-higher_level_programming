742bece821144eca889e348055fb520c90f89a60
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-07-20 02:05:56 +0300

    task3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,14 @@
+-- Creates table: force_name on mysql server
+
+-- If force_name already exist; script should not fail
+-- force_name:
+--    id (INT)
+--    name (VARCHAR) : NOT NULL
+
+-- The db name will be passed as an arg of mysql cmd
+
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
