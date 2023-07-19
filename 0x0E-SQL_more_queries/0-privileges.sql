ae79551c93705387965280b79c8d2b31f6c294a6
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-07-20 00:33:26 +0300

    privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- List privileges of mysql users user_0d_1 and user_0d_2 on localhost
+-- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
