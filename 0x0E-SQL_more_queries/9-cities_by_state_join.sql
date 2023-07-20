145348750c919589c986b3f3d5f431328063707e
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-07-20 04:08:44 +0300

    task9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,5 +4,5 @@ USE hbtn_0d_usa;
 
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.states_id = states.id
-ORDER BY cities.id ASC;
+NNER JOIN states ON cities.states_id = states.id
+ORDER BY cities.id cities.id;
