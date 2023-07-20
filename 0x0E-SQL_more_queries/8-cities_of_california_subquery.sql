2104634ed96b22c421f4658d15bdd0e8cbd7d08f
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-07-20 04:01:58 +0300

    task8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,11 +4,10 @@
 --    name = Calix
 --    id (INT)
 
-USE hbtn_0d_usa;
-
-SELECT * FROM cities
-WHERE state_id = (
-    SELECT id FROM states
-    WHERE name = 'Carlifornia'
-)
-ORDER BY id ASC;
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
