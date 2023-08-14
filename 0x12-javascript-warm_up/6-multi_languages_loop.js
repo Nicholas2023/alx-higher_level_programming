57a74b28db29d3105c10f3f1636e3c315f3a8a94
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-08-15 00:49:17 +0300

    task6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Create an array of lines
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Loop throught the array
+for (const line of lines) {
+  console.log(line);
+}
