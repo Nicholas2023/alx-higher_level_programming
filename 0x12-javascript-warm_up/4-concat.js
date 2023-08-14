111bbd036a6e63c4c7ecf53a005e9b03d58d524e
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-08-15 00:28:51 +0300

    task4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Get the first and second arguments
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+// Print the arguments using console.log(...)
+console.log(firstArg + ' is ' + secondArg);
