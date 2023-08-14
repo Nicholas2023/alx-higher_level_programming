511d19bf87589476873043578bfced53eed88c42
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-08-15 02:43:53 +0300

    task101

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Define the function callMeMoby
+function callMeMoby (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+// Export the callMeMoby function
+module.exports = {
+  callMeMoby
+};
