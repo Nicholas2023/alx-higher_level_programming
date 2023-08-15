4209e6078045f605dea4641e53a380d7cf8b03f3
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-08-15 15:04:13 +0300

    task1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Defined class Rectangle
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+// Export the module
+module.exports = Rectangle;
