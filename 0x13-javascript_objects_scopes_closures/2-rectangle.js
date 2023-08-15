48752b0a4ec46a401942105dd12006e964ca1823
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-08-15 15:42:29 +0300

    task2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,9 +4,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
-      return;
     } else if (w === undefined || h === undefined) {
-      return;
     } else {
       this.width = w;
       this.height = h;
