6fe86e0a2f752d97f26557fc6494aeb8c7653dc2
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-08-16 00:21:53 +0300

    task102

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+if (process.argv.length !== 5) {
+  console.error('Usage: ./102-concat.js <file1> <file2> <destination>');
+  process.exit(1);
+}
+
+const file1Path = process.argv[2];
+const file2Path = process.argv[3];
+const destinationPath = process.argv[4];
+
+try {
+  const file1Content = fs.readFileSync(file1Path, 'utf8');
+  const file2Content = fs.readFileSync(file2Path, 'utf8');
+  const concatenatedContent = file1Content + '\n' + file2Content;
+
+  fs.writeFileSync(destinationPath, concatenatedContent);
+  console.log('Files concatenated successfully.');
+} catch (error) {
+  console.error('Error:', error.message);
+  process.exit(1);
+}
