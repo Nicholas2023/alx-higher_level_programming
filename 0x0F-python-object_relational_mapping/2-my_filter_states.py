0a20f7586d6e0d0cf5de18c6ef2ade62ca8e174f
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-08-18 08:10:52 +0300

    task2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Display all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """
+    Get MySQL credentials and state name from cmd line args
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    """
+    Connect to MySQL server
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    """
+    Create a cursor object to interact with the database
+    """
+    cursor = db.cursor()
+
+    """
+    Create and execute the SQL query with user input
+    """
+    query = "SELECT * FROM `states` WHERE name LIKE BINARY %s ORDER BY `id`"
+    cursor.execute(query, (state_name,))
+
+    """
+    Fetch all the results
+    """
+    results = cursor.fetchall()
+
+    """
+    Display the results
+    """
+    for row in results:
+        print(row)
+
+    """
+    Close the cursor and database connection
+    """
+    cursor.close()
+    db.close()
