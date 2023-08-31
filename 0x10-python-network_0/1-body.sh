495db424b8fbc3b4be065686d4cb2cb356385e8c
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-09-01 00:43:02 +0300

    task1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL and displays the response body
-
-# Send a GET request using curl with the following options:
-# -s: Silent mode, no progress output
-# -f: Fail silently on HTTP errors
-# -L: Follow redirects
-# -X GET: Explicitly set the request method to Get
 curl -sfL "$1" -X GET
