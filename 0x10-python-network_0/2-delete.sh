c05b9cc01f27e1ccc4144875e28e83c2d92e5d43
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-09-01 00:47:52 +0300

    task2

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Sends a DELETE request to the URL provided as an arg ($1)
-
-# Send a DELETE request using curl with the following options:
-# -s: Silent mode, no progress output
-# -X DELETE: Explicitly set the request method to DELETE
 curl -s "$1" -X DELETE
