89709d62143cda3d756b7d361afd7da076e45ab6
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-09-02 02:49:37 +0300

    task1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,20 +3,12 @@
 A script that takes URL and displays the req id
 """
 
-import urllib.request
 import sys
+import urllib.request
 
-if __name__ == '__main__':
-    """Get the url from the cmd line args"""
-    _url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-    """Use a with statement to open the URL and manage
-        the connection
-    """
-
-    with urllib.request.urlopen(_url) as response:
-        # Get the value of the 'X-Request-ID' header from the response
-        x_request_id = response.headers.get('X-Request-Id')
-
-        # Print the value of the x-Request:
-        print(dict(response.headers).get("x_request_id"))
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
