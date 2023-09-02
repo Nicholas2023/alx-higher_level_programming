4cf1491d98ad876c335e9031493ef13de0543302
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-09-02 04:04:09 +0300

    task6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,14 +11,8 @@ if __name__ == "__main__":
     """
     Get the URL and email address from command line arguments
     """
-    _url = sys.argv[1]
-    email = sys.argv[2]
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
 
-    """ Create a dictiopnary with the email parameter """
-    data = {'email': email}
-
-    """ Send a POST request to the URL with the email parameter """
-    response = requests.post(_url, data=data)
-
-    """ Display the response body """
-    print("Your email is:", response.text)
+    r = requests.post(url, data=value)
+    print(r.text)
