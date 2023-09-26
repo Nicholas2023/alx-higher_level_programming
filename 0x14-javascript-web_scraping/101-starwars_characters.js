0f7d96057c1f0211f7b5165836a3cbc1423e6a41
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-09-26 23:51:56 +0300

    task7

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,38 +1,26 @@
 #!/usr/bin/node
 
-// Import the 'request' module to make HTTP requests
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-// Get the Movie ID from the command-line arguments
-const movieId = process.argv[2];
+function printCharacters (characters, idx) {
+  request(characters[idx], (err, res, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.parse(body).name);
+      if (idx + 1 < characters.length) {
+        printCharacters(characters, idx + 1);
+      }
+    }
+  });
+}
 
-// Define the URL to the Star Wars API for the movie with the given ID
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-// Make a GET request to the specified URL
-request(url, (error, response, body) => {
-  if (error) {
-    // If an error occurs during the request, print the error
-    console.error(error);
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
   } else {
-    // Parse the JSON response to extract the characters data
-    const movieData = JSON.parse(body);
-
-    // Create an array to store character URLs in the same order
-    const characterUrls = movieData.characters;
-
-    // Loop through chacracter URL and make requests to retrieve character names
-    characterUrls.forEach((characterUrl) => {
-      request(characterUrl, (charError, charResponse, charBody) => {
-        if (charError) {
-          // If an error occurs during character request, print the error
-          console.error(charError);
-        } else {
-          // Parse the JSON response to extract the character's name
-          const characterData = JSON.parse(charBody);
-          console.log(characterData.name);
-        }
-      });
-    });
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,29 +1,24 @@
 #!/usr/bin/node
 
-// Import the 'request' module to make HTTP request
 const request = require('request');
+const url = process.argv[2];
 
-// Get the API URL from the command-line arguments
-const apiUrl = process.argv[2];
-
-// Character ID for Wedge Antilles
-const characterId = 18;
-
-// Make a GET request to the Star Wars API URL
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    // If an error occurs during the request, print the error
-    console.error(error);
-  } else {
-    // Parse the JSON response to extract the films data
-    const filmsData = JSON.parse(body).results;
-
-    // Filter the films where Wedge Antilles is present (character ID 18)
-    const count = filmsData.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    ).length;
-
-    // Print the number of films where Wedge Antilles is present
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const filmIndex in films) {
+      const filmChars = films[filmIndex].characters;
+      for (const charIndex in filmChars) {
+        if (filmChars[charIndex].includes('18')) {
+          count++;
+        }
+      }
+    }
     console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
